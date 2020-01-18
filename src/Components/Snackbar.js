520f97aaf4d5e61d7b9b70fdc492d685ce2c799c
Anton Kanugalawattage <antondilon@gmail.com>
2020-01-18 15:06:11 -0500

    snackbar intro

---
+++ src/Components/Snackbar.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import Snackbar from '@material-ui/core/Snackbar';
+import SnackbarContent from '@material-ui/core/SnackbarContent';
+
+class  SimpleSnackbar extends Component {
+  state={
+      open: true
+  }
+  
+  render(){
+    setTimeout(() => {
+        this.setState({
+            open:false
+          });
+      }, 6000);
+      
+  return (
+    <div>
+            <Snackbar
+                style={{fontFamily:'Avenir, Nunito Sans, sans-serif', fontWeight:'900'}}
+                anchorOrigin={{vertical: 'bottom', horizontal: 'right'}}
+                variant="error"
+                autoHideDuration={6000}
+                open={this.state.open}
+                >
+                <SnackbarContent style={{backgroundColor:'#f5f5f5',fontFamily:'Avenir, Nunito Sans, sans-serif', fontWeight:'900', fontSize: 16, color:'black'}}
+                message="Made By Anton Kanugalawattage, 2020"
+                />
+            </Snackbar>
+    </div>
+  );
+}
+}
+
+export default SimpleSnackbar
