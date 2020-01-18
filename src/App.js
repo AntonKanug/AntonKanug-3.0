2a3af416e218e0da0f30fee989e387a68232b7cf
Anton Kanugalawattage <antondilon@gmail.com>
2020-01-18 15:06:01 -0500

    scrollable using nav

--- src/App.js
+++ src/App.js
@@ -1,15 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar'
 import Intro from './Components/Intro'
+import Projects from './Components/Projects'
+import Footer from './Components/Footer'
 import './index.css';
+import ScrollableAnchor from 'react-scrollable-anchor';
+import LinearProgress from '@material-ui/core/LinearProgress';
 
-function App() {
-  return (
+
+const Sections = () => {
+  const anchors = ['About', 'Projects'];
+  const sectionsArray = [<Intro />,  <Projects />];
+  return (anchors.map((item, index) => { return (<ScrollableAnchor id={item} key={index}><div>{sectionsArray[index]}</div></ScrollableAnchor>) }));
+}
+
+class App extends Component {
+  state={
+    open: 'inherit'
+}
+
+render() {
+    setTimeout(() => {
+    this.setState({
+        open:'none'
+        });
+    }, 2000);
+
+    return (
     <div style={{fontFamily:'SFPro'}}>
-    <Intro />
+      <LinearProgress style={{display:this.state.open,  height:5}} />
+      <div id="main" className="main-wrapper">
+        <Navbar />
+        <Sections/>
+        <Footer />
+      </div>
+
     </div>
   );
+}
 }
 
 export default App;
