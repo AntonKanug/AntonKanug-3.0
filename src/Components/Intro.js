7e4d6ef054dc7f45103aab579ef1b45e613aee97
Anton Kanugalawattage <antondilon@gmail.com>
2020-01-18 10:46:49 -0500

    components

---
+++ src/Components/Intro.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react'
+import './Intro.scss';
+import NavBar from './Navbar';
+import Projects from './Projects'
+import LinearProgress from '@material-ui/core/LinearProgress';
+
+require('bootstrap')
+export class Intro extends Component {
+    state={
+        open: 'inherit'
+    }
+
+    render() {
+        setTimeout(() => {
+        this.setState({
+            open:'none'
+            });
+        }, 2000);
+
+        return (
+            <div>
+                 <LinearProgress style={{display:this.state.open, height:5}} />
+            <div id="main" className="main-wrapper">
+                <NavBar />
+            <div className="container"  style={{marginTop:'50px'}}>
+                <div className="row align-items-center">
+                    <div className="col-lg-6 col-sm-6 col-xs-6">
+                        <div className="banner-content slide-in-blurred-left" >
+                            <div>
+                            <h1 style={{fontFamily:'SFProBold'}}>
+                            <span role="img" aria-label="Hand">👋</span> Hello!
+                            </h1>
+                            <p style={{fontFamily:'SFProLight'}}>
+                            My name is <span style={{fontFamily:'SFProSB'}}>Anton Kanugalawattage </span>📚 💻 I'm a student at McMaster University studying Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.    
+                            <span style={{fontFamily:'SFProSB'}}> Software Engineering</span>.
+                            </p>
+                            </div>
+                        </div>
+
+
+                        <div className="cont" style={{background:'#f5f5f5', marginTop: '0px', borderRadius:'10px', padding:'10px', width:'250px', height:'80px'}}>
+                                <ul class="list-inline" >
+                                    <div class="social-media-icon"  style={{display: 'inline-block', padding:5}}>
+                                        <li class="social-git">
+                                            <a href="https://github.com/AntonKanug" target="_blank"><img src="assets/github.svg" data-toggle="tooltip" style={{width:45, height:45}} title="GitHub"/></a>
+                                        </li>
+                                    </div>
+                                    <div class="social-media-icon"  style={{display: 'inline-block', padding:5}}> 
+                                        <li class="social-linkedin">
+                                            <a href="https://www.linkedin.com/in/antonkanug/" target="_blank"><img src="assets/linkedin.svg" data-toggle="tooltip" style={{width:45, height:45}} title="LinkedIn"/></a>
+                                        </li>
+                                    </div>
+                                    <div class="social-media-icon"  style={{display: 'inline-block', padding:5}}>
+                                        <li class="social-email">
+                                            <a href="#contact" ><img src="assets/email.svg" data-toggle="tooltip" style={{width:45, height:45}} title="Contact"/></a>
+                                        </li>
+                                    </div>
+                                    <div class="social-media-icon"  style={{display: 'inline-block', padding:5}}>
+                                        <li class="social-resume">
+                                            <a href="assets/AK-Resume.pdf"  target="_blank"><img src="assets/cv.svg" data-toggle="tooltip" style={{width:45, height:45}} title="Resume" /></a>
+                                        </li>
+                                    </div>
+                                </ul>
+                            </div>
+
+
+
+                    </div>
+                    <div className="col-lg-6 col-sm-6 col-xs-6 slide-in-blurred-bottom" >
+                        <img src="assets/img-landing.svg" className='me' alt="loading..." style={{paddingTop:'10px'}} />
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="main" className="project-wrapper" >
+        <Projects />
+        </div>
+        </div>
+        )
+    }
+}
+
+export default Intro
---
+++ src/Components/Navbar.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import './Navbar.scss';
+
+var MobileMenuStyle = {
+    height: "0px"
+}
+
+// var MobileMenuIcon = {
+//     visibility: false
+// }
+
+// var MobileCloseIcon = {
+//     isHidden: true
+// }
+
+const MobileMenu = (props) => {
+    return (
+        <div className="nav-menu" style={MobileMenuStyle}>
+            <a href="">
+                <div className="resume-link">
+                    View Resume <i className="fas fa-chevron-right"></i>
+                </div>
+            </a>
+            <Anchors />
+        </div>
+    );
+}
+
+const Anchors = () => {
+    const anchorsArr = ["Projects", "Experience", "About"];
+    return (anchorsArr.map(item => { return (<a key={item} href={`#${item}`}>{item}</a>) }));
+}
+
+export default class Header extends Component {
+    state = {
+        showMobileMenu: false,
+        showButton: true,
+    }
+
+    clickedMenu = () => {
+        this.setState({ showMobileMenu: true });
+        MobileMenuStyle = { height: "100%" };
+        this.setState({ showButton: false });
+    }
+
+    clickedMenuClose = () => {
+        this.setState({ showMobileMenu: false });
+        MobileMenuStyle = { height: "0px;" };
+        this.setState({ showButton: true });
+    }
+
+    render() {
+
+        return (
+            <div className="nav-bar" style={{fontFamily:'SFProBold'}}>
+                <div className="container">
+                    <div className="row" >
+                        <div className="col">
+                            <div className="logo slide-in-blurred-top">
+                                <span className="full-name">
+                                    <h1 style={{fontFamily:'SFProBold', letterSpacing:'0.7px'}}>Anton Kanugalawattage</h1>
+                                </span>
+                                <span className="short-name">
+                                    <h1>Anton</h1>
+                                </span>
+                            </div>
+                        </div>
+                        <div className="col slide-in-blurred-right" style={{fontFamily:'SFProLight', fontSize:'18px'}}>
+                            <div className="full-options">  
+                                <a href="">
+                                    <div className="resume-options" style={{fontFamily:'SFProBold'}}>
+                                    📜 View Resume<i style={{paddingLeft:'5px'}}class="fas fa-arrow-right"></i>
+                                    </div>
+                                </a>
+                                <Anchors />
+                            </div>
+                            <div className="short-options">
+                                <i className={this.state.showButton ? "fas fa-bars menu-icon" : "fa fa-bars hidden"} onClick={this.clickedMenu} />
+                                <i className={!this.state.showButton ? "fas fa-times menu-icon" : "fa fa-times hidden"} onClick={this.clickedMenuClose} />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {this.state.showMobileMenu ? <MobileMenu /> : null}
+            </div>
+        );
+    }
+}
